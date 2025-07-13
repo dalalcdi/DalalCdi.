48e4f6649f77a7a056c3497b1fa0c0c09f461eac
dalalcdi <ytpr232323@gmail.com>
2025-07-13 22:54:40 +0600

    Update main.js

--- main.js
+++ main.js
@@ -11,11 +11,17 @@ counterRef.on('value', (snapshot) => {
 });
 
 function incrementCounter() {
+    // Increment counter in Firebase
     counterRef.transaction((currentValue) => {
         return (currentValue || 0) + 1;
     });
-}
 
+    // Slap animation trigger
+    const slipper = document.getElementById('shoeSeal');
+    slipper.classList.remove('slap'); // Reset in case it's still animating
+    void slipper.offsetWidth; // Force reflow
+    slipper.classList.add('slap');    // Re-add animation
+}
 function convertToBangla(num) {
     const banglaDigits = ['০','১','২','৩','৪','৫','৬','৭','৮','৯'];
     return num.toString().split('').map(d => banglaDigits[d] || d).join('');
